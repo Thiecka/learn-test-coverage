2b4b7ec70c594b9f58efbdbd0685ed67288244cd
Die Aissatou Dieng <dieaissatou.dieng@segula.fr>
2023-07-03 15:05:15 +0200

    erreu3

--- erreur.js
+++ erreur.js
@@ -5,3 +5,10 @@ sum ++;
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Bonjour');
+  });
